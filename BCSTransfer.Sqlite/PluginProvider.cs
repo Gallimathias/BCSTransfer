8ba6f22594db841b1d1e9ef5adc6b11ca66e5a8a
Maximilian Krüger <Maximilian.Krueger@airsphere.de>
2019-07-19 17:43:13 +0200

    Add Sqlite with new Plugin structure
    * Add sqlite mit plugin output and sqlite database logic

---
+++ BCSTransfer.Sqlite/PluginProvider.cs
@@ -0,0 +1,47 @@
+﻿using BCSTransfer.Core;
+using BCSTransfer.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCSTransfer.Sqlite
+{
+    public class PluginProvider : IPluginProvider
+    {
+        public string PluginNamespace => "SQLITE";
+
+        public void Register(ITypeContainer typeContainer, string pluginNamespace)
+        {
+            typeContainer.Register<IDatabaseProvider, DatabaseProvider>();
+
+            typeContainer.Register<IDatabase, SqliteDatabase>(InstanceBehaviour.Singleton);
+            typeContainer.Register<SqliteDatabase, SqliteDatabase>(InstanceBehaviour.Singleton);
+        }
+
+        public Task Initalize()
+        {
+            return Task.CompletedTask;
+        }        
+
+        public Task UnInitalize()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private class DatabaseProvider : IDatabaseProvider
+        {
+            public DbContextOptions GetDatabaseContext(string source)
+            {
+                var builder = new DbContextOptionsBuilder();
+                builder.UseSqlite($"Data Source={source}");
+                return builder.Options;
+            }
+        }
+    }
+}
---
+++ BCSTransfer.Sqlite/SqliteDatabase.cs
@@ -0,0 +1,33 @@
+﻿using BCSTransfer.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BCSTransfer.Sqlite
+{
+    class SqliteDatabase : Database
+    {
+        static SqliteDatabase() 
+            => SQLitePCL.Batteries.Init();
+
+        public SqliteDatabase(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public override IDatabase GetEnsureDatabase(string source)
+        {
+            var db = GetDatabase(source) as Database;
+            db.Database.EnsureCreated();
+            return db;
+        }
+
+        public static IDatabase GetDatabase(string source)
+        {
+            var builder = new DbContextOptionsBuilder();
+            builder.UseSqlite($"Data Source={source}");
+            return new SqliteDatabase(builder.Options);
+        }
+    }
+}
